78e1573028a99e2104bccc4f09b826117f7b8c34
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-12-04 20:28:28 -0500

    setting up readability pull

--- linkterceptor.php
+++ linkterceptor.php
@@ -62,9 +62,105 @@ class linkterceptor {
 		$link = $_POST['url'];
 		$link_title = $_POST['link_title'];
 		
+		set_time_limit(0);
+		
+		$url = $this->de_https($url);
+		$url = str_replace('&amp;','&', $url);
+		
+		$request = wp_remote_get( $link, array('timeout' => '30') );
+		if (is_wp_error($request)) {
+			$content = 'error-secured';
+			//print_r($request); die();
+			return $content;
+		}
+		if ( ! empty( $request['body'] ) ){
+			$html = $request['body'];
+		
+			if ($link_title == ''){
+				$title = getTitle($html);
+			}
+
+			$content = $this->readability_object($html);
+			
+			
+		} else {
+			$content = false;
+			return $content;
+		}	
+
+		//Readability imp from work with CHNM.
+				
+		
 		die();
 	
 	}
+	
+	function getTitle($content){
+		if(strlen($str)>1){
+			preg_match("/\<title\>(.*)\<\/title\>/",$str,$title);
+			return $title[1];
+		}
+	}
+
+	
+	# Tries to turn any HTTPS URL into an HTTP URL for servers without ssl configured.
+	public function de_https($url) {
+		$urlParts = parse_url($url);
+		if (in_array('https', $urlParts)){
+			$urlParts['scheme'] = 'http';
+			$url = $urlParts['scheme'] . '://'. $urlParts['host'] . $urlParts['path'] . $urlParts['query'];
+		}
+		return $url;
+	}
+
+	
+		//Readability imp from work with CHNM.
+	# The function that runs a URL through Readability and attempts to give back the plain content.
+	public function readability_object($html) {
+	//ref: http://www.keyvan.net/2010/08/php-readability/
+		
+		//check if tidy exists to clean up the input.
+		if (function_exists('tidy_parse_string')) {
+			$tidy = tidy_parse_string($html, array(), 'UTF8');
+			$tidy->cleanRepair();
+			$html = $tidy->value;
+		}
+		// give it to Readability
+		$readability = new Readability($html, $url);
+
+		// print debug output?
+		// useful to compare against Arc90's original JS version -
+		// simply click the bookmarklet with FireBug's
+		// console window open
+		$readability->debug = false;
+
+		// convert links to footnotes?
+		$readability->convertLinksToFootnotes = false;
+
+		// process it
+		$result = $readability->init();
+
+		if ($result){
+			$content = $readability->getContent()->innerHTML;
+			//$content = $contentOut->innerHTML;
+				//if we've got tidy, let's use it.
+				if (function_exists('tidy_parse_string')) {
+					$tidy = tidy_parse_string($content,
+						array('indent'=>true, 'show-body-only'=>true),
+						'UTF8');
+					$tidy->cleanRepair();
+					$content = $tidy->value;
+				}
+
+		} else {
+			# If Readability can't get the content, send back a FALSE to loop with.
+			$content = false;
+		}
+
+		return $content;
+
+	}
+		
 
 }
 
